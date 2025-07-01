a2e89128a49930231476dee0c7f7d700ca20be49
K-Wutthichai-7 <getthai.13.2540@gmail.com>
2025-07-01 20:12:51 +0700

    up to date

--- express_crud_sqlsever/config/db.js
+++ express_crud_sqlsever/config/db.js
@@ -5,16 +5,26 @@
 const sql = require('mssql/msnodesqlv8');
 
 //create a database configuration
+// var config = {
+//    server: "ACER\\SQLEXPRESS", // e.g., 'DESKTOP-mjsi\\MSSQLEXPRESS'
+//    database: "pos_system",
+// //    user: 'sa', // please read above note
+// //    password: "admin@12345", // please read above note
+//    options: {
+//        trustedConnection: true,
+//    },
+//    driver: "msnodesqlv8",
+// };
+
 var config = {
-   server: "ACER\\SQLEXPRESS", // e.g., 'DESKTOP-mjsi\\MSSQLEXPRESS'
-   database: "pos_system",
-//    user: 'sa', // please read above note
-//    password: "admin@12345", // please read above note
-   options: {
-       trustedConnection: true,
-   },
-   driver: "msnodesqlv8",
+    server: "ACER\\SQLEXPRESS", // ชื่อเซิร์ฟเวอร์และ instance name ของ SQL Server
+    database: "pos_system", // ชื่อฐานข้อมูล
+    options: {
+        trustedConnection: true // ใช้ Windows Authentication
+    },
+    driver: "msnodesqlv8" // ระบุไดรเวอร์ที่ใช้งาน
 };
+
 
 //note: please make double (\\) before your instance name
 //if you get confused then please watch the video link at the bottom you can see in details about this
